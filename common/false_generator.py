beed98cca10df2bfa7704b66039e211ac9cc18b5
Erik Bryhn Myklebust <erik.bryhn.myklebust@gmail.com>
2019-09-25 20:33:52 +0200

    Update false_generator.py
    Some optimisations.

--- common/false_generator.py
+++ common/false_generator.py
@@ -17,12 +17,22 @@ max and min cardinality makes no sense to use. Voiding max will make more true t
 from rdflib import Graph, Literal, BNode
 from rdflib.namespace import RDFS, OWL, RDF
 from collections import defaultdict
+import warnings
 
 from random import choice
 
 
 class FalseGenerator:
     def __init__(self, tboxfile, aboxfile):
+        """
+        Create inconsitent triples based on an ontology. 
+        
+        args
+            tboxfile :: string
+                path to TBox file, rdf/owl format
+            aboxfile :: string 
+                path to ABox file, any format supported by rdflib
+        """
         
         self.ontology = Graph()
         self.ontology.parse(tboxfile)
@@ -70,31 +80,33 @@ class FalseGenerator:
         for p,c in self.ontology.subject_objects(OWL.equivalentClass):
             self.disjoint[p] |= self.disjoint[c]
             
-    def corrupt_entity(self, p, e, method = 'disjoint'):
+    def _corrupt_entity(self, p, e, method = 'disjoint'):
         
         if method == 'disjoint':
+            """
+            Find classes disjoint to C(e). The choose a random entity from the union of these classes.
+            """
             cls = self.data.objects(subject=e, predicate=RDF.type)
             disjoint_entities = set()
             for c in cls:
-                for dc in self.disjoint[c]:
-                    disjoint_entities |= self.data.subjects(predicate=RDF.type, object=dc)
+                disjoint_entities |= set.union(*[self.data.subjects(predicate=RDF.type, object=dc) for dc in self.disjoint[c]])
                     
             if disjoint_entities:
                 e = choice(list(disjoint_entities))
             else:
-                return self.corrupt_entity(p, e, 'random')
+                e = None
             
         elif method == 'range':
             try:
                 e = choice(list(self.entities.difference(self.R[p])))
             except KeyError:
-                return self.corrupt_entity(p,e,'random')
+                e = None
             
         elif method == 'domain':
             try:
                 e = choice(list(self.entities.difference(self.D[p])))
             except KeyError:
-                return self.corrupt_entity(p,e,'random')
+                e = None
         
         elif method == 'random':
             e = choice(list(self.entities))
@@ -141,7 +153,7 @@ class FalseGenerator:
                 return self.corrupt(None, method, ignore_literals, ignore_blank_nodes)
         
         if 'domain' in method:
-            s = self.corrupt_entity(p,s,'domain')
+            s = self._corrupt_entity(p,s,'domain')
             
             if isinstance(s, Literal): #subject cannot be literal
                 return self.corrupt((s,p,o), method, ignore_literals, ignore_blank_nodes)
@@ -149,7 +161,7 @@ class FalseGenerator:
                 return self.corrupt((s,p,o), method, ignore_literals, ignore_blank_nodes)
         
         if 'range' in method:
-            o = self.corrupt_entity(p,o,'range')
+            o = self._corrupt_entity(p,o,'range')
             
             if ignore_literals and isinstance(o,Literal):
                 return self.corrupt((s,p,o), method, ignore_literals, ignore_blank_nodes)
@@ -158,13 +170,21 @@ class FalseGenerator:
      
         if 'disjoint' in method:
             if 'range' in method:
-                o = self.corrupt_entity(p,o,'disjoint')
+                o = self._corrupt_entity(p,o,'disjoint')
             if 'domain' in method:
-                s = self.corrupt_entity(p,s,'disjoint')
+                s = self._corrupt_entity(p,s,'disjoint')
             
             if (ignore_literals and isinstance(o,Literal)) or isinstance(s,Literal):
                 return self.corrupt((s,p,o), method, ignore_literals, ignore_blank_nodes)
             if ignore_blank_nodes and (isinstance(o,BNode) or isinstance(s,BNode)):
                 return self.corrupt((s,p,o), method, ignore_literals, ignore_blank_nodes)
+        
+        if 'random' in method:
+            s = choice(list(self.entities))
+            o = choice(list(self.entities))
+           
+        if not all([s,p,o]):
+            warnings.warn('No corrupt entity found: returning random triple')
+            return self.corrupt(None, method=['random'], ignore_literals, ignore_blank_nodes)
         
         return s,p,o
